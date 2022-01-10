66d208ea89a806cd3655e9ced03da3501a10a8ce
1642399386@qq.com <1642399386@qq.com>
2022-01-10 23:07:49 +0800

    小地图

---
+++ End/Assets/OHZ/OHZ_Scripts/MiniMap/MiniMapController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MiniMapController : MonoBehaviour
+{
+    //整合 —— 如果场景中没有敌人则显示小地图，有敌人则不显示（玩家附身对象不计入其中）
+    public GameObject miniMap;
+    bool miniMapSwitch = false;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.M))
+        {
+            miniMapSwitch = !miniMapSwitch;
+        }
+        miniMap.SetActive(miniMapSwitch);
+    }
+}
---
+++ End/Assets/OHZ/OHZ_Scripts/MiniMap/MiniMapRoom.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MiniMapRoom : MonoBehaviour
+{
+    public bool isInThisRoom = false;//现在是否在房间内,玩家所在房间高亮显示
+    public bool haveComeThisRoom = false;//是否来过该房间，只有来过才显示房间边框
+}
