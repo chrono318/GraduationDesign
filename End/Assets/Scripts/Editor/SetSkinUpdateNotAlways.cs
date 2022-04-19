9871d260d09fda496650bfee89fb96ba0d2eb06f
Chrono <1796453790@qq.com>
2022-04-19 21:55:12 +0800

    设置所有spriteSkin 的alwaysUpdate为false

---
+++ End/Assets/Scripts/Editor/SetSkinUpdateNotAlways.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.U2D.Animation;
+
+public class SetSkinUpdateNotAlways
+{
+    [MenuItem("Tools/Set Skin UpdateAlways False")]
+    static void SetSkinUpdate()
+    {
+        Scene curScene = SceneManager.GetActiveScene();
+        foreach (GameObject go in curScene.GetRootGameObjects())
+        {
+            SetSkinUpdateNotAlwaysIngo(go);
+        }
+        EditorUtility.DisplayDialog("修改spriteSkin", "Done", "Done");
+    }
+    static void SetSkinUpdateNotAlwaysIngo(GameObject go)
+    {
+        if (go.GetComponent<SpriteSkin>())
+        {
+            go.GetComponent<SpriteSkin>().alwaysUpdate = false;
+        }
+        else
+        {
+            for (int i = 0; i < go.transform.childCount; i++)
+            {
+                SetSkinUpdateNotAlwaysIngo(go.transform.GetChild(i).gameObject);
+            }
+        }
+    }
+}
