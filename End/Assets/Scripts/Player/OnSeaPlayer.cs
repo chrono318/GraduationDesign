d7efcde95debf88120607a374ef327e2c24709dd
1642399386@qq.com <1642399386@qq.com>
2022-05-03 18:05:34 +0800

    先提交没搞完

--- End/Assets/Scripts/Player/OnSeaPlayer.cs
+++ End/Assets/Scripts/Player/OnSeaPlayer.cs
@@ -7,18 +7,19 @@ using UnityEngine.UI;
 public class OnSeaPlayer : MonoBehaviour
 {
     public float speed = 10f;
-    public Text aimText;
-    public Text tipsText;
+    //public Text aimText;
+    //public Text tipsText;
     bool isSelect;
     string scenceName;
+    Animator anim;
 
+    private void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
 
     void Update()
     {
-        //float h = Input.GetAxisRaw("Horizontal");
-        //float v = Input.GetAxisRaw("Vertical");
-        //float t = Time.deltaTime;
-        //transform.position += new Vector3(speed * h * t, speed * v * t, 0);
         Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
         transform.position = new Vector3(mousePos.x, mousePos.y, 0);
         if(isSelect && Input.GetMouseButtonDown(0))
@@ -32,11 +33,12 @@ public class OnSeaPlayer : MonoBehaviour
         if (collision.gameObject.CompareTag("Level"))
         {
             collision.gameObject.transform.Find("Frame").gameObject.SetActive(true);
-            aimText.gameObject.transform.parent.gameObject.SetActive(true);
-            this.aimText.text = collision.gameObject.GetComponent<ChangeText>().aimText;
-            this.tipsText.text = collision.gameObject.GetComponent<ChangeText>().tipsText;
+            //aimText.gameObject.transform.parent.gameObject.SetActive(true);
+            //this.aimText.text = collision.gameObject.GetComponent<ChangeText>().aimText;
+            //this.tipsText.text = collision.gameObject.GetComponent<ChangeText>().tipsText;
             this.scenceName = collision.gameObject.GetComponent<ChangeText>().scenceName;
             isSelect = true;
+            anim.Play("select");
         }
     }
 
@@ -45,7 +47,7 @@ public class OnSeaPlayer : MonoBehaviour
         if (collision.gameObject.CompareTag("Level"))
         {
             collision.gameObject.transform.Find("Frame").gameObject.SetActive(false);
-            aimText.gameObject.transform.parent.gameObject.SetActive(false);
+            //aimText.gameObject.transform.parent.gameObject.SetActive(false);
             isSelect = false;
         }
     }
